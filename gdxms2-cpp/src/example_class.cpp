bdf78db63227d93b2fd23063d9a7a0ecb8013dff
MoTrix <xmoezkingx@gmail.com>
2026-05-31 15:01:08 +0100

    Link miniscript2 to GDExtension

--- gdxms2-cpp/src/example_class.cpp
+++ gdxms2-cpp/src/example_class.cpp
@@ -4,6 +4,9 @@ void ExampleClass::_bind_methods() {
 	godot::ClassDB::bind_method(D_METHOD("print_type", "variant"), &ExampleClass::print_type);
 }
 
+ExampleClass::ExampleClass() : interp() {}
+
+
 void ExampleClass::print_type(const Variant &p_variant) const {
 	print_line(vformat("Type: %d", p_variant.get_type()));
 	print_line("hello!");
--- gdxms2-cpp/src/example_class.hpp
+++ gdxms2-cpp/src/example_class.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Interpreter.g.h"
+
 #include "godot_cpp/classes/ref_counted.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
 #include "godot_cpp/variant/variant.hpp"
@@ -9,11 +11,14 @@ using namespace godot;
 class ExampleClass : public RefCounted {
 	GDCLASS(ExampleClass, RefCounted)
 
+private:
+	MiniScript::Interpreter interp;
+
 protected:
 	static void _bind_methods();
 
 public:
-	ExampleClass() = default;
+	ExampleClass();
 	~ExampleClass() override = default;
 
 	void print_type(const Variant &p_variant) const;
